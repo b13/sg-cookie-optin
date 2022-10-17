6f894e59b38a716819378a761d1ace45c07ace8b
Georgi Mateev <gmateev@exactag.com>
2022-10-17 15:02:25 +0300

    [TASK] #158 Cookie List plugin

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -11,3 +11,11 @@
 	'CookieList',
 	'LLL:EXT:sg_cookie_optin/Resources/Private/Language/locallang_backend.xlf:cookieListPluginLabel'
 );
+
+$pluginSignature = 'sgcookieoptin_cookielist';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    // FlexForm configuration schema file
+    'FILE:EXT:sg_cookie_optin/Configuration/FlexForms/CookieList.xml'
+);
