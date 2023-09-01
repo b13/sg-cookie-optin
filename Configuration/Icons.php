c8ebdeffb072e2b9d3033fb801d779f8025df896
Kevin von Spiczak <kevin.spiczak@sgalinski.de>
2023-09-01 14:12:57 +0200

    [TASK] add debugging hint

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Important! Do not return a variable named $icons, because it will result in an error.
+ * The core requires this file and then the variable names will clash.
+ * Either use a closure here, or do not call your variable $icons.
+ */
+
 $iconList = [];
 foreach (['ext-sg_cookie_optin' => 'extension-sg_cookie_optin.svg'] as $identifier => $path) {
 	$iconList[$identifier] = [
