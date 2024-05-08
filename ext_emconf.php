d194468720f2e2b39689e05409bb69d61bdf2df3
Stefan Galinski <stefan@sgalinski.de>
2024-05-08 21:03:31 +0200

    [TASK] Release version 5.5.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
 		on our website: https://www.sgalinski.de/en/typo3-products-web-development/cookie-optin-for-typo3/.
 	',
 	'category' => 'module',
-	'version' => '5.5.2',
+	'version' => '5.5.3',
 	'state' => 'stable',
 	'uploadfolder' => FALSE,
 	'createDirs' => '',
