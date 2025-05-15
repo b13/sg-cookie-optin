3039e60ade25e1efed6d969487bdea5a1c440458
Georgi Mateev <georgi.mateev@sgalinski.de>
2025-05-16 00:02:15 +0300

    [TASK] Release version 7.0.3

--- Classes/Service/LicenceCheckService.php
+++ Classes/Service/LicenceCheckService.php
@@ -170,12 +170,13 @@ class LicenceCheckService {
     '7.0.0' => 1746715432, // 2025-05-08T14:43:52Z
     '7.0.1' => 1747063092, // 2025-05-12T15:18:12Z
     '7.0.2' => 1747224364, // 2025-05-14T12:06:04Z
+    '7.0.3' => 1747342932, // 2025-05-15T21:02:12Z
 ];
 
 	/**
 	 * The current extension version
 	 */
-	public const CURRENT_VERSION = '7.0.2';
+	public const CURRENT_VERSION = '7.0.3';
 
 	/**
 	 * @param mixed $validUntil A timestamp, which says the lifetime of this key.
