3772494b7790e4f8a10ff17e17542053d8537faf
Georgi Mateev <georgi.mateev@sgalinski.de>
2025-05-16 00:01:14 +0300

    [BUGFIX] Restore storage pid field to plugin configurations

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -12,6 +12,15 @@ call_user_func(function () {
 		'sg_cookie_optin'
 	);
 
+	// Include pages (storage pid) field in the tt_content TCA for the Cookie Optin plugin
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+		'tt_content',
+		'pages,',
+		'sgcookieoptin_optin',
+		'after:subheader'
+	);
+
+
 	// Register the CookieList plugin as a content element (CType)
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
 		[
@@ -33,7 +42,7 @@ call_user_func(function () {
 	// Include the FlexForm field in the tt_content TCA for the CookieList plugin
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
 		'tt_content',
-		'--div--;Configuration,pi_flexform,',
+		'--div--;Configuration,pi_flexform,pages,',
 		'sgcookieoptin_cookielist',
 		'after:subheader'
 	);
