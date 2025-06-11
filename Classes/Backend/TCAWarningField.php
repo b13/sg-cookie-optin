9a0114e801b92548c4969175e8948e961b1139cd
Kevin Ditscheid <kevin.ditscheid@sgalinski.de>
2025-06-11 12:49:53 +0200

    [TASK] Change the layout of alert boxes to proper callout boxes with icons

--- Classes/Backend/TCAWarningField.php
+++ Classes/Backend/TCAWarningField.php
@@ -41,7 +41,19 @@ class TCAWarningField extends AbstractFormElement {
 	public function render(): array {
 		$result = $this->initializeResultArray();
 		$message = LocalizationUtility::translate('backend.warning.overwritten_checkbox', 'sg_cookie_optin');
-		$result['html'] = '<div class="alert alert-info">' . $message . '</div>';
+		$result['html'] = '
+<div class="callout callout-info">
+	<div class="media">
+		<div class="media-left">
+			<span class="icon-emphasized">
+				<core:icon identifier="actions-info" />
+			</span>
+		</div>
+		<div class="media-body">
+			' . $message . '
+		</div>
+	</div>
+</div>';
 		return $result;
 	}
 }
